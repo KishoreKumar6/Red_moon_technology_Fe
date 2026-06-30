852be51849b41eb0058e6746289df93cae5c6684
LeastActionCompany <leastactioncompany@gmail.com>
2026-07-01 00:00:30 +0530

    renamed for images

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../../assets/Redmoontechnology1.png";
+import logo from "../../assets/RedMoonTechnology1.png";
 
 export default function Footer() {
   return (
--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { Menu, X } from "lucide-react";
-import logo from "../../assets/Redmoontechnology1.png";
+import logo from "../../assets/RedMoonTechnology1.png";
 const links = [
   "Home",
   "About",
